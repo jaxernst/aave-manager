abbe285afcb343e5abffe4782dafe01a7b471f7a
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-01-05 01:39:49 -0500

    Add offchain keeper

---
+++ keeper/README.md
@@ -0,0 +1,15 @@
+# Keeper
+
+Simple script to monitor the ETH2X Aave position, and rebalance if the leverage ratio is outside of the acceptable range. Intended to be run on a cron job.
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run src/index.ts
+```
---
+++ keeper/src/index.ts
@@ -0,0 +1,39 @@
+import {
+  createWalletClient,
+  http,
+  parseAbi,
+  publicActions,
+  type Hex,
+} from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { base } from 'viem/chains'
+
+const client = createWalletClient({
+  account: privateKeyToAccount(Bun.env.DEPLOYER_KEY as Hex),
+  transport: http(Bun.env.BASE_RPC_URL),
+  chain: base,
+}).extend(publicActions)
+
+const contract = {
+  address: '0x' as const,
+  abi: parseAbi([
+    'function rebalance() public',
+    'function getLeverageRatio() public view returns (uint256)',
+  ]),
+}
+
+const leverageRatio = await client.readContract({
+  ...contract,
+  functionName: 'getLeverageRatio',
+})
+
+if (leverageRatio < 1.9e18 || leverageRatio > 2.1e18) {
+  await client.writeContract({
+    ...contract,
+    functionName: 'rebalance',
+  })
+
+  console.log('Rebalanced')
+} else {
+  console.log('Leverage ratio is within acceptable range')
+}
